88f6bd1980d8bc5da0fb90d005d6ad13877a372e
abdallah-shehawey <shehawey9@gmail.com>
2025-08-10 15:42:49 +0300

    Add Femto shell task

---
+++ femtoshell.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int femtoshell_main(int argc, char *argv[]) {
+    char line[11000];
+    const char *prompt = "FemtoShell> ";
+    int last_status = 0;
+
+    while (1) {
+        printf("%s", prompt);
+        fflush(stdout);
+
+        if (fgets(line, sizeof(line), stdin) == NULL) {
+            break;
+        }
+
+        size_t len = strlen(line);
+        if (len > 0 && line[len - 1] == '\n') {
+            line[len - 1] = '\0';
+            len--;
+        }
+
+        if (len == 0) {
+            last_status = 0;
+            continue;
+        }
+
+        if (strcmp(line, "exit") == 0) {
+            printf("Good Bye\n");
+            return 0;
+        }
+
+        if (strncmp(line, "echo", 4) == 0 && (line[4] == ' ' || line[4] == '\0')) {
+            if (line[4] == ' ') {
+                printf("%s\n", line + 5);
+            } else {
+                printf("\n");
+            }
+            last_status = 0;
+            continue;
+        }
+
+        printf("Invalid command\n");
+        last_status = 1; // فشل
+    }
+
+    return last_status;
+}
+
